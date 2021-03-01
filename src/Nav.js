d012a18449cf63301ea437087e0df25befc661ae
윤석 한 <hys6078@naver.com>
2021-03-01 16:16:17 +0900

    add sidebar scrollbar

--- src/Nav.js
+++ src/Nav.js
@@ -44,54 +44,52 @@ export default function Nav(){
                     <div className="game-list">
                         <h3>A~Z</h3>
                         <ul>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
+                            <li>EA SPORTS FIFA ONLINE 4</li>
+                            <li>V4</li>
                         </ul>
-                        <h3>A~Z</h3>
+                        <h3>ㄱ~ㄹ</h3>
                         <ul>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
+                            <li>던전앤파이터</li>
+                            <li>드래곤네스트</li>
                         </ul>
-                        <h3>A~Z</h3>
+                        <h3>ㅁ~ㅅ</h3>
                         <ul>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
-                        </ul>
-                        <h3>A~Z</h3>
-                        <ul>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
+                            <li>마비노기</li>
+                            <li>마비노기영웅전</li>
+                            <li>메이플스토리</li>
+                            <li>메이플스토리2</li>
+                            <li>바람의나라</li>
+                            <li>버블파이터</li>
+                            <li>사이퍼즈</li>
+                            <li>서든어택</li>
                         </ul>
                     </div>
                     <div className="game-list">
-                        <h3>A~Z</h3>
+                        <h3>ㅇ~ㅈ</h3>
                         <ul>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
+                            <li>아스가르드</li>
+                            <li>아키에이지</li>
+                            <li>어둠의전설</li>
+                            <li>엘소드</li>
+                            <li>일랜시아</li>
                         </ul>
-                        <h3>A~Z</h3>
+                        <h3>ㅊ~ㅋ</h3>
                         <ul>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
+                            <li>천애명월도</li>
+                            <li>카운터스트라이크온라인</li>
+                            <li>카트라이더</li>
+                            <li>카트라이더: 드리프트</li>
+                            <li>커즈펠</li>
+                            <li>크레이지아케이드</li>
+                            <li>클로저스</li>
                         </ul>
-                        <h3>A~Z</h3>
+                        <h3>ㅌ~ㅎ</h3>
                         <ul>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
+                            <li>테일즈런너</li>
+                            <li>테일즈위버</li>
+                            <li>트리오브세이비어</li>
+                            <li>파이널판타지14</li>
+                            <li>프리스타일2</li>
                         </ul>
                     </div>
                 </div>
