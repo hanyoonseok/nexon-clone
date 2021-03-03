04f8af11c95da2cae9a4ef2c75d0b454fb9c6f02
윤석 한 <hys6078@naver.com>
2021-03-03 21:46:37 +0900

    add cardmode img

--- src/CardMode.js
+++ src/CardMode.js
@@ -18,11 +18,23 @@ export default function CardMode(){
             </div>
             <div className="all-pc-card">
                 <div className="card-img">
-                    <img alt="" src="sources/v4.jpg"/>
+                    <img alt="" src="sources/kart.jpg"/>
                 </div>
                 <div className="card-info">
-                    <div className="card-title"><span>V4</span><span><i class="fas fa-coffee"></i></span></div>
-                    <div className="card-detail">대장, PC에서 기다릴게요!</div>
+                    <div className="card-title"><span>카트라이더</span><span><i class="fas fa-coffee"></i></span></div>
+                    <div className="card-detail">크레이지레이싱 카트라이더</div>
+                </div>
+                <div className="card-tag">
+                    <i class="fas fa-desktop"> 레이싱</i>
+                </div>
+            </div>
+            <div className="all-pc-card">
+                <div className="card-img">
+                    <img alt="" src="sources/fifa.jpg"/>
+                </div>
+                <div className="card-info">
+                    <div className="card-title"><span>EA SPORTS FIFA ONLINE 4</span><span><i class="fas fa-coffee"></i></span></div>
+                    <div className="card-detail">축구, 그 이상을 경험할 수 있는 최고의 방법</div>
                 </div>
                 <div className="card-tag">
                     <i class="fas fa-desktop"> MMORPG</i>
@@ -30,74 +42,62 @@ export default function CardMode(){
             </div>
             <div className="all-pc-card">
                 <div className="card-img">
-                    <img alt="" src="sources/v4.jpg"/>
+                    <img alt="" src="sources/sudden.png"/>
                 </div>
                 <div className="card-info">
-                    <div className="card-title"><span>V4</span><span><i class="fas fa-coffee"></i></span></div>
-                    <div className="card-detail">대장, PC에서 기다릴게요!</div>
+                    <div className="card-title"><span>서든어택</span><span><i class="fas fa-coffee"></i></span></div>
+                    <div className="card-detail">신규 폭파미션 마린 베이스 오픈</div>
                 </div>
                 <div className="card-tag">
-                    <i class="fas fa-desktop"> MMORPG</i>
+                    <i class="fas fa-desktop"> FPS</i>
                 </div>
             </div>
             <div className="all-pc-card">
                 <div className="card-img">
-                    <img alt="" src="sources/v4.jpg"/>
+                    <img alt="" src="sources/deon.jpg"/>
                 </div>
                 <div className="card-info">
-                    <div className="card-title"><span>V4</span><span><i class="fas fa-coffee"></i></span></div>
-                    <div className="card-detail">대장, PC에서 기다릴게요!</div>
+                    <div className="card-title"><span>던전앤파이터</span><span><i class="fas fa-coffee"></i></span></div>
+                    <div className="card-detail">나이트 진각성 업데이트</div>
                 </div>
                 <div className="card-tag">
-                    <i class="fas fa-desktop"> MMORPG</i>
+                    <i class="fas fa-desktop"> ACTION RPG</i>
                 </div>
             </div>
             <div className="all-pc-card">
                 <div className="card-img">
-                    <img alt="" src="sources/v4.jpg"/>
+                    <img alt="" src="sources/cheon.jpg"/>
                 </div>
                 <div className="card-info">
-                    <div className="card-title"><span>V4</span><span><i class="fas fa-coffee"></i></span></div>
-                    <div className="card-detail">대장, PC에서 기다릴게요!</div>
+                    <div className="card-title"><span>천애명월도</span><span><i class="fas fa-coffee"></i></span></div>
+                    <div className="card-detail">뛰어난 그래픽과 스토리의 대작 무협 RPG</div>
                 </div>
                 <div className="card-tag">
-                    <i class="fas fa-desktop"> MMORPG</i>
+                    <i class="fas fa-desktop"> RPG</i>
                 </div>
             </div>
             <div className="all-pc-card">
                 <div className="card-img">
-                    <img alt="" src="sources/v4.jpg"/>
+                    <img alt="" src="sources/crazy.jpg"/>
                 </div>
                 <div className="card-info">
-                    <div className="card-title"><span>V4</span><span><i class="fas fa-coffee"></i></span></div>
-                    <div className="card-detail">대장, PC에서 기다릴게요!</div>
+                    <div className="card-title"><span>크레이지아케이드</span><span><i class="fas fa-coffee"></i></span></div>
+                    <div className="card-detail">#누구나#쉽게#캐주얼#인싸게임</div>
                 </div>
                 <div className="card-tag">
-                    <i class="fas fa-desktop"> MMORPG</i>
+                    <i class="fas fa-desktop"> 캐주얼</i>
                 </div>
             </div>
             <div className="all-pc-card">
                 <div className="card-img">
-                    <img alt="" src="sources/v4.jpg"/>
+                    <img alt="" src="sources/maple.jpg"/>
                 </div>
                 <div className="card-info">
-                    <div className="card-title"><span>V4</span><span><i class="fas fa-coffee"></i></span></div>
-                    <div className="card-detail">대장, PC에서 기다릴게요!</div>
+                    <div className="card-title"><span>메이플스토리</span><span><i class="fas fa-coffee"></i></span></div>
+                    <div className="card-detail">모험과 재미가 가득한 2D 횡스크롤 MMORPG</div>
                 </div>
                 <div className="card-tag">
-                    <i class="fas fa-desktop"> MMORPG</i>
-                </div>
-            </div>
-            <div className="all-pc-card">
-                <div className="card-img">
-                    <img alt="" src="sources/v4.jpg"/>
-                </div>
-                <div className="card-info">
-                    <div className="card-title"><span>V4</span><span><i class="fas fa-coffee"></i></span></div>
-                    <div className="card-detail">대장, PC에서 기다릴게요!</div>
-                </div>
-                <div className="card-tag">
-                    <i class="fas fa-desktop"> MMORPG</i>
+                    <i class="fas fa-desktop"> RPG</i>
                 </div>
             </div>
         </div>
--- src/MCardMode.js
+++ src/MCardMode.js
@@ -7,7 +7,7 @@ export default function MCardMode(){
             <div className="m-card-list">
                 <div className="m-card">
                     <div>
-                        <img alt="" src="sources/kart.jpg"/>
+                        <img alt="" src="sources/mmaple.jpg"/>
                     </div>
                     <div className="m-card-detail">
                         <div>
@@ -22,12 +22,12 @@ export default function MCardMode(){
                 </div>
                 <div className="m-card">
                     <div>
-                        <img alt="" src="sources/kart.jpg"/>
+                        <img alt="" src="sources/mbaram.jpg"/>
                     </div>
                     <div className="m-card-detail">
                         <div>
-                            <div className="card-title"><span>메이플스토리M</span><span><i class="fas fa-coffee"></i></span></div>
-                            <div className="card-detail">RPG</div>
+                            <div className="card-title"><span>바람의나라:연</span><span><i class="fas fa-coffee"></i></span></div>
+                            <div className="card-detail">MMORPG</div>
                         </div>
                         <div className="m-card-logo">
                             <i class="fab fa-apple"></i>
@@ -37,12 +37,12 @@ export default function MCardMode(){
                 </div>
                 <div className="m-card">
                     <div>
-                        <img alt="" src="sources/kart.jpg"/>
+                        <img alt="" src="sources/mkart.jpg"/>
                     </div>
                     <div className="m-card-detail">
                         <div>
-                            <div className="card-title"><span>메이플스토리M</span><span><i class="fas fa-coffee"></i></span></div>
-                            <div className="card-detail">RPG</div>
+                            <div className="card-title"><span>카트라이더 러쉬플러스</span><span><i class="fas fa-coffee"></i></span></div>
+                            <div className="card-detail">레이싱</div>
                         </div>
                         <div className="m-card-logo">
                             <i class="fab fa-apple"></i>
@@ -52,12 +52,12 @@ export default function MCardMode(){
                 </div>
                 <div className="m-card">
                     <div>
-                        <img alt="" src="sources/kart.jpg"/>
+                        <img alt="" src="sources/mfifa.png"/>
                     </div>
                     <div className="m-card-detail">
                         <div>
-                            <div className="card-title"><span>메이플스토리M</span><span><i class="fas fa-coffee"></i></span></div>
-                            <div className="card-detail">RPG</div>
+                            <div className="card-title"><span>EA SPORTS FIFA MOBILE</span><span><i class="fas fa-coffee"></i></span></div>
+                            <div className="card-detail">스포츠</div>
                         </div>
                         <div className="m-card-logo">
                             <i class="fab fa-apple"></i>
@@ -67,12 +67,12 @@ export default function MCardMode(){
                 </div>
                 <div className="m-card">
                     <div>
-                        <img alt="" src="sources/kart.jpg"/>
+                        <img alt="" src="sources/mv4.png"/>
                     </div>
                     <div className="m-card-detail">
                         <div>
-                            <div className="card-title"><span>메이플스토리M</span><span><i class="fas fa-coffee"></i></span></div>
-                            <div className="card-detail">RPG</div>
+                            <div className="card-title"><span>V4</span><span><i class="fas fa-coffee"></i></span></div>
+                            <div className="card-detail">MMORPG</div>
                         </div>
                         <div className="m-card-logo">
                             <i class="fab fa-apple"></i>
@@ -82,12 +82,12 @@ export default function MCardMode(){
                 </div>
                 <div className="m-card">
                     <div>
-                        <img alt="" src="sources/kart.jpg"/>
+                        <img alt="" src="sources/mcounter.png"/>
                     </div>
                     <div className="m-card-detail">
                         <div>
-                            <div className="card-title"><span>메이플스토리M</span><span><i class="fas fa-coffee"></i></span></div>
-                            <div className="card-detail">RPG</div>
+                            <div className="card-title"><span>카운터사이드</span><span><i class="fas fa-coffee"></i></span></div>
+                            <div className="card-detail">수집형RPG</div>
                         </div>
                         <div className="m-card-logo">
                             <i class="fab fa-apple"></i>
@@ -97,12 +97,12 @@ export default function MCardMode(){
                 </div>
                 <div className="m-card">
                     <div>
-                        <img alt="" src="sources/kart.jpg"/>
+                        <img alt="" src="sources/mtraha.png"/>
                     </div>
                     <div className="m-card-detail">
                         <div>
-                            <div className="card-title"><span>메이플스토리M</span><span><i class="fas fa-coffee"></i></span></div>
-                            <div className="card-detail">RPG</div>
+                            <div className="card-title"><span>트라하</span><span><i class="fas fa-coffee"></i></span></div>
+                            <div className="card-detail">MMORPG</div>
                         </div>
                         <div className="m-card-logo">
                             <i class="fab fa-apple"></i>
@@ -112,12 +112,12 @@ export default function MCardMode(){
                 </div>
                 <div className="m-card">
                     <div>
-                        <img alt="" src="sources/kart.jpg"/>
+                        <img alt="" src="sources/mcrazy.png"/>
                     </div>
                     <div className="m-card-detail">
                         <div>
-                            <div className="card-title"><span>메이플스토리M</span><span><i class="fas fa-coffee"></i></span></div>
-                            <div className="card-detail">RPG</div>
+                            <div className="card-title"><span>크레이지아케이드 BnB M</span><span><i class="fas fa-coffee"></i></span></div>
+                            <div className="card-detail">캐주얼</div>
                         </div>
                         <div className="m-card-logo">
                             <i class="fab fa-apple"></i>
